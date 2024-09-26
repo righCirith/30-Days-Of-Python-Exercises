32a4654f35673da76d39aca2b4f7fccfbba525d8
Zeroth <172555825+righCirith@users.noreply.github.com>
2024-09-25 23:26:52 -0600

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,40 @@
+#Exercise: Level 2
+#1
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+#2
+print('Name')
+print('FamilyName')
+print('Country')
+print('I am enjoying 30 days of python')
+
+#4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Name'))
+print(type('Surname'))
+print(type('Country'))
+
+#Exercise: Level 3
+#1
+print(8)
+print(3.2)
+print(89 + 1j)
+print('Hi')
+print(True)
+print(['l', 'i', 's', 't'])
+print(('t', 'u', 'p', 'l', 'e'))
+print({'s', 'e', 't'})
+print({
+    'name': 'Name',
+    'age':999999
+})
