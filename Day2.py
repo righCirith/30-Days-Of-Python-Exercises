22d801f3b06a131f8ff980a638b18152c842161d
Zeroth <172555825+righCirith@users.noreply.github.com>
2024-09-27 00:27:28 -0600

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,64 @@
+#Day 2: 30 Days of python programming
+
+#Exercise : Level 1
+first_name = 'Name'
+last_name = 'Last Name'
+full_name = first_name + ' ' + last_name
+country = 'country'
+city = 'city'
+age = 100
+year = 2024
+is_married = False
+is_true = True
+is_light_on = False
+x, y, z = 1, 2, 3
+
+#Exercise : Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(len(first_name))
+print('first name lenght is:', len(first_name), 'and', 'last name length is:', len(last_name))
+
+x = 5
+y = 4
+total = x + y
+diff = x - y
+product = x * y
+division = x / y
+reminder  = x % y
+exp = x ** y
+floor_division = x // y
+
+
+#r = 30
+
+#r muste be > 0
+r = float(input('Type the radius of the circle (muste be greater than 0): '))
+pi = 3.1416
+a = pi*(r**2) 
+c = 2*pi*r
+d = 2*r
+print('Area = ', a)
+print('Circunference = ', c)
+print('Diameter = ', d)
+
+user_first_name = input('Type your first name: ')
+user_last_name = input('Type your last name: ')
+user_country = input('Type your country: ')
+user_age = input('Type your age: ')
+
+print('User full name is:', user_first_name, user_last_name)
+
+#help('keywords')
